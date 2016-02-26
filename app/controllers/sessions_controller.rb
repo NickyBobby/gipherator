876571b72277f399ba44abd47b29fbd89b9b1af2
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-25 20:40:53 -0700

    add login and logout functionality user can login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      flash[:errors] = "Invalid Login"
+      flash.now[:errors] = "Invalid Login"
       render :new
     end
   end
-  # def destroy
-  #   session.clear
-  #   redirect_to root_path
-  # end
+
+  def new
+
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:show, :index, :new, :create]
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 end
