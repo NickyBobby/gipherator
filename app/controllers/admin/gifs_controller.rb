1b4d863d25e4e91475f81d72f99492f4c4a1c80e
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-27 16:56:49 -0700

    add api juicer module
    pull out method in controller to api juicer--the juiciest part is the module.

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -2,20 +2,13 @@ require "faraday"
 require "json"
 
 class Admin::GifsController < ApplicationController
-
+  include ApiJuicer
   def index
-    # Hit Giphy API and return sample of random GIFs
-    # gifs = Gif.all
     @gif = Gif.new
   end
-  #
+  
   def create
-    url = "http://api.giphy.com/v1/gifs/search?q=#{params[:gif][:category]}&api_key=dc6zaTOxFJmzC"
-    response = Faraday.get(url)
-    raw_data = response.body
-    data = JSON.parse(raw_data)
-    image_path = data["data"].sample["images"]["fixed_height"]["url"]
-
+    image_path = find_by_keyword(params[:gif][:category])
     @gif = Gif.create(name: params[:gif][:category], image_path: image_path)
     redirect_to current_user
   end
