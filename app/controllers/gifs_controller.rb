54753a5e97a3c004436f8177505fa20151246a70
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-02-28 18:14:54 -0700

    users can fav/unfav + not add the same gif twice

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,12 +1,19 @@
 class GifsController < ApplicationController
 
   def update
-    if current_user
-      current_user.gifs << Gif.find(params[:id])
+    gif = Gif.find(params[:id])
+    if current_user && !current_user.gifs.include?(gif)
+      current_user.gifs << gif
       redirect_to root_path
     else
+      flash[:error] = "#{gif.name} is already a favorite"
       redirect_to root_path
     end
+  end
+
+  def destroy
+    current_user.gifs.destroy(params[:id])
+    redirect_to current_user
   end
 
 end
--- spec/features/regular_user_can_mark_favorite_gifs_spec.rb
+++ spec/features/regular_user_can_mark_favorite_gifs_spec.rb
@@ -27,4 +27,58 @@ RSpec.feature "Regular user can mark favorite GIFs" do
     expect(page).to have_content "Nickrinna"
   end
 
+  scenario "User can unfavorite a GIF" do
+    user = User.create(username: "Nick", password: "password")
+    gif_path = "http://gph.is/1W4DBfX"
+    gif = Gif.create(name: "Kazoo Kid", image_path: gif_path)
+    gif2 = Gif.create(name: "Nickrinna", image_path: gif_path)
+    gif3 = Gif.create(name: "The Chad", image_path: gif_path)
+
+    user.gifs << gif
+
+    visit root_path
+    click_on "Login"
+    fill_in "Username", with: "Nick"
+    fill_in "Password", with: "password"
+    click_on "Login"
+    expect(page).to have_content "Kazoo Kid"
+    click_on "Back to Home"
+    click_on "Add The Chad to Favorites"
+    click_on "Add Nickrinna to Favorites"
+    expect(3).to eq user.gifs.count
+    click_on "View my Gifs"
+    click_on "Remove Nickrinna from Favorites"
+    click_on "Remove Kazoo Kid from Favorites"
+    expect(1).to eq user.gifs.count
+    expect(page).to have_content "The Chad"
+    expect(page).not_to have_content "Nickrinna"
+    expect(page).not_to have_content "Kazoo Kid"
+
+    click_on "Back to Home"
+    expect(page).to have_content "Nickrinna"
+    expect(page).to have_content "Kazoo Kid"
+
+
+  end
+
+  scenario "User cannot favorite the same GIF twice" do
+    user = User.create(username: "Nick", password: "password")
+    gif2 = Gif.create(name: "Nickrinna", image_path: "gif_path")
+
+    visit root_path
+    click_on "Login"
+    fill_in "Username", with: "Nick"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    click_on "Back to Home"
+
+    click_on "Add Nickrinna to Favorites"
+    expect(1).to eq user.gifs.count
+
+    click_on "Add Nickrinna to Favorites"
+    expect(page).to have_content "Nickrinna is already a favorite"
+    expect(1).to eq user.gifs.count
+  end
+
 end
